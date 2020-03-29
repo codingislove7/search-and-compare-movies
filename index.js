9451985957bacdf139edf4ea106f49f1a2bf2cde
Behnam Sheykhe <learndlt@gmail.com>
2020-03-29 21:37:38 +0430

    add input and inject it to fetch request

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const fetchData = async () => {
+const fetchData = async searchTerm => {
   const response = await axios.get("http://www.omdbapi.com/", {
     params: {
       apikey: "683df0d6",
-      s: "avengers"
+      s: searchTerm
     }
   });
   console.log(response.data);
 };
 fetchData();
+const input = document.querySelector("input");
+input.addEventListener("input", event => {
+  fetchData(event.target.value);
+});
