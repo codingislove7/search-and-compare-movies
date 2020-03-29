ac27bec58043cfd4cb9eb21839bcefcb0e27b7fd
Behnam Sheykhe <learndlt@gmail.com>
2020-03-29 11:44:13 +0430

    add api request

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Hi there!');
+const fetchData = async () => {
+  const respnse = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "683df0d6",
+      s: "avengers"
+    }
+  });
+};
