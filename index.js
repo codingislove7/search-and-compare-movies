bdd36e40c7903204c8193b3798468d864b546e48
Behnam Sheykhe <learndlt@gmail.com>
2020-03-30 08:18:14 +0430

    render css in js

--- index.js
+++ index.js
@@ -12,32 +12,35 @@ const fetchData = async searchTerm => {
 };
 fetchData();
 
-const root = document.querySelector(".autocomplete");
-
+const root = document.querySelector('.autocomplete');
 root.innerHTML = `
-<label><b>Search For a Movie</b></label>
-<input class = "input" />
-<div class = "dropdown>
-<div class = "dropdown-menu>
-<div class = "dropdown-content results"></div>
-</div>
-</div>
+  <label><b>Search For a Movie</b></label>
+  <input class="input" />
+  <div class="dropdown">
+    <div class="dropdown-menu">
+      <div class="dropdown-content results"></div>
+    </div>
+  </div>
 `;
 
-const input = document.querySelector("input");
-const dropdown = document.querySelector(".dropdown");
-const resultsWrapper = document.querySelector(".results");
+const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
 
 const onInput = async event => {
   const movies = await fetchData(event.target.value);
+
+  dropdown.classList.add('is-active');
   for (let movie of movies) {
-    const div = document.createElement("div");
-    div.innerHTML = `
-    <img src = "${movie.Poster}" />
-    <h1>${movie.Title}</h1>
+    const option = document.createElement('a');
+
+    option.classList.add('dropdown-item');
+    option.innerHTML = `
+      <img src="${movie.Poster}" />
+      ${movie.Title}
     `;
-    document.querySelector("#target").appendChild(div);
+
+    resultsWrapper.appendChild(option);
   }
 };
-
-input.addEventListener("input", debounce(onInput, 500));
+input.addEventListener('input', debounce(onInput, 500));
