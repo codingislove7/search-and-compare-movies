207f16ca323f20d227e7113a8bb53a1ac6b35ad9
Behnam Sheykhe <learndlt@gmail.com>
2020-03-30 15:38:49 +0430

    update input entry when user click on a movie

--- index.js
+++ index.js
@@ -43,7 +43,11 @@ const onInput = async event => {
       <img src="${imgSrc}" />
       ${movie.Title}
     `;
-
+    option.addEventListener("click", () => {
+      dropdown.classList.remove("is-active");
+      input.value = movie.Title;
+      onMovieSelect();
+    });
     resultsWrapper.appendChild(option);
   }
 };
