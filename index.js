9681d2364cd558e4811a25b55bc86b0e674afe08
Behnam Sheykhe <learndlt@gmail.com>
2020-03-30 15:53:56 +0430

    extract imbd id and send an other request for receive more info

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const onInput = async event => {
     option.addEventListener("click", () => {
       dropdown.classList.remove("is-active");
       input.value = movie.Title;
-      onMovieSelect();
+      onMovieSelect(movie);
     });
     resultsWrapper.appendChild(option);
   }
@@ -57,3 +57,12 @@ document.addEventListener("click", event => {
     dropdown.classList.remove("is-active");
   }
 });
+const onMovieSelect = async movie => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "683df0d6",
+      i: movie.imdbID
+    }
+  });
+  console.log(response.data);
+};
